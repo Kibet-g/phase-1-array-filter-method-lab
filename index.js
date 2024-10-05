b65f4061c6f93a7c96a92cd7401313924e2d9f7e
+ <kibetg984@gmail.com>
2024-10-05 08:51:04 +0300

    Made changes on the fuzzyMatch function

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  // 2. fuzzyMatch function
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+  }
+  
+  // 3. matchName function
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
